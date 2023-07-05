b2027ac4fadfb404c298da6787e99f4c6b2de855
devbobnwaka <nwakachibueze@gmail.com>
2023-07-05 15:40:10 +0100

    settings for deployment

--- settings/production.py
+++ settings/production.py
@@ -1,20 +1,22 @@
 from .base import *
-
+import dj_database_url
 
 ALLOWED_HOSTS = []
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('NAME'), 
-        'USER': os.environ.get('USER'), 
-        'PASSWORD': os.environ.get('PASSWORD'),
-        'HOST': os.environ.get('HOST'), 
-        'PORT': os.environ.get('PORT'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.environ.get('NAME'), 
+#         'USER': os.environ.get('USER'), 
+#         'PASSWORD': os.environ.get('PASSWORD'),
+#         'HOST': os.environ.get('HOST'), 
+#         'PORT': os.environ.get('PORT'),
+#     }
+# }
+
+DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 
 # Base url to serve media files
 MEDIA_URL = '/media/'
